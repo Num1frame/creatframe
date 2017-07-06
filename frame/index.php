1109301e3b32ab7e7b25560104309fa7e4810b26
liujiang135 <1392534029@qq.com>
2017-07-07 00:58:53 +0800

    'asdqwe'

--- frame/index.php
+++ frame/index.php
@@ -2,8 +2,13 @@
 define('CSS_PATH', '/frame/public/css/');
 define('JS_PATH', '/frame/public/js/');
 define('IMG_PATH', '/frame/public/img/');
-define('VIEW_PATH', 'app/views/');
 define('PHP_PATH', '/frame/index.php/');
+//define('CSS_PATH', '/creatframe/frame/public/css/');
+//define('JS_PATH', '/creatframe/frame/public/js/');
+//define('IMG_PATH', '/creatframe/frame/public/img/');
+//define('PHP_PATH', '/creatframe/frame/index.php/');
+
+define('VIEW_PATH', 'app/views/');
 define('DEBUG', true);
 if (DEBUG) {
     include 'core/debug.php';
