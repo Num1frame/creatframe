6382e07b36da4a39059735ad37ab57ef9657bbf7
1479465488 <1479465488@qq.com>
2017-07-07 16:58:53 +0800

    aaaa

--- frame/app/grd_grzl.php
+++ frame/app/grd_grzl.php
@@ -4,6 +4,8 @@ use core\core;
 
 class grd_grzl extends core{
     function index(){
+        $this->assign('data1',M('fensi')->selectALL());
+        $this->assign('data', M('guanzhu')->selectAll());
         $this->assign('title','个人资料');
         $this->display('grd_grzl');
 
