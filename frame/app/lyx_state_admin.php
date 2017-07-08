07a8cffe412bd19f45b5cb9dc3f01de5f2da90a0
1213152524 <1213152524@qq.com>
2017-07-08 17:41:38 +0800

    l

--- frame/app/lyx_state_admin.php
+++ frame/app/lyx_state_admin.php
@@ -17,4 +17,25 @@ class lyx_state_admin extends core{
         $data=M("state")->selectAll();
         echo json_encode($data);
     }
+    function add(){
+        $atmt=M()->pdo->prepare("insert into state (name,title,content,img,uid,num1,num2,num3) VALUES (?,?,?,?,?,0,0,0)");
+        $atmt->bindValue(1,$_REQUEST['name']);
+        $atmt->bindValue(2,$_REQUEST['title']);
+        $atmt->bindValue(3,$_REQUEST['content']);
+        $atmt->bindValue(4,$_REQUEST['img']);
+        $atmt->bindValue(5,$_REQUEST['radiobutton']);
+        $atmt->execute();
+    }
+    function delete(){;
+        $data=M()->query("delete from state where id={$_REQUEST['id']}");
+    }
+    function update(){
+        $ziduan=$_REQUEST['ziduan'];
+        $val=$_REQUEST['val'];
+        $id=$_REQUEST['id'];
+        $atmt=M()->pdo->prepare("update state set {$ziduan}= ? where id = ?");
+        $atmt->bindValue(1,$val);
+        $atmt->bindValue(2,$id);
+        $atmt->execute();
+    }
 }
