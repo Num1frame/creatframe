e7662d364aaea88c2efea1a5857ab5bb3d99ba53
taozi <1160068047@qq.com>
2017-07-09 22:45:41 +0800

    b1.0

--- frame/app/zjt_designmanage.php
+++ frame/app/zjt_designmanage.php
@@ -11,6 +11,7 @@
          $this->assign('title', '设计师管理');
          $this->display('zjt_designmanage');
      }
+     //展示数据
      function load(){
          $data=M()->query('select * from zjt_design ORDER BY id DESC ');
          echo json_encode($data);
@@ -19,25 +20,21 @@
          M()->query("delete from zjt_design WHERE id='{$_REQUEST['id']}'");
      }
 
-
      //添加
 
-     function add(){
-         print_r($_FILES);
+     function zjt_add(){
          $src=$_FILES['pic']['tmp_name'];
          $ext=explode('.',$_FILES['pic']['name'])[1];
          $file_name=md5(time()).'.'.$ext;
-         $dist='Public/upload/' .$file_name;
-         $name='/Public/upload/' .$file_name;
+         $dist='public/upload/' .$file_name;
+         $name='/creatframe/frame/public/upload/' .$file_name;
          move_uploaded_file($src,$dist);
-         include 'Core/Db.php';
-         $store=new Db();
-         $stmt =$store->pdo->prepare('insert into zjt_design (name,pic,content) values (?,?,?)');
-         $stmt->bindValue(1,$_REQUEST['name']);
-         $stmt->bindValue(2,$name);
-         $stmt->bindValue(3,$_REQUEST['content']);
-         $stmt->execute();
-         $id=$store->pdo->lastInsertId();
-         echo json_encode($id);
+         $stmt =M()->query("insert into zjt_design (name,pic,content) values ('$_REQUEST[name]','$name','$_REQUEST[content]')");
+     }
+     function zjt_up(){
+         $key=$_REQUEST['key'];
+         $value=$_REQUEST['value'];
+         $id=$_REQUEST['id'];
+         $data=M()->query("update zjt_design set ('{$key}'='{$value}' where id={$id}) ");
      }
  }
--- frame/public/js/zjt_designmanage.js
+++ frame/public/js/zjt_designmanage.js
@@ -16,6 +16,113 @@ $(function () {
             let el = `
                     <tr data-id="${v.id}">
                         <td>${v.id}</td>
+                        <td>
+                            <input type="" class="name" value="${v.name}">
+                        </td>
+                        <td>
+                            <img src="${v.pic}" alt="" style="height: 60px;width: 100px;">
+                        </td>
+                        <td>
+                            <input type="" class="content" value="${v.content}">
+                        </td>
+                        <td>
+                            <a class="zjt_user_del" href="#">删除</a>
+                        </td>
+                    </tr>
+                `
+            $(el).appendTo(tbodyEl);
+        })
+    }
+
+    $.ajax({
+        url: "../zjt_designmanage/load",
+        success: function (data) {
+            data=JSON.parse(data);  //将字符串转化为数组
+            render(data);
+        }
+    });
+
+    tbodyEl.on('click', '.zjt_user_del', function () {
+        let parid = $(this).closest('tr').attr('data-id');
+        $.ajax({
+            url: '../zjt_designmanage/del?id=' + parid,
+            success: function () {
+                location.reload();
+            }
+        })
+        return false;
+    })
+
+    //添加
+    $("#submit").on('click',function(){
+        var form=$('form').serializeArray();
+        var formData = new FormData($('#form').get(0));
+        $.ajax({
+            url:'../zjt_designmanage/zjt_add',
+            processData: false,
+            contentType: false,
+            type: 'post',
+            data: formData,
+            success:function(data){
+                location.reload();
+                location.href=location.pathname + '#list';
+            }
+        })
+        return false;
+    })
+    $('input[name=pic]').on('change',function(){
+        $(this).prev().html($(this).val());
+    })
+
+    //修改
+    tbodyEl.on('change','.name,.content',function(){
+        value=$(this).val();
+        id=$(this).closest('td').parent('tr').attr('data-id');
+        key=$(this).attr('class');
+        $.ajax({
+            url:'../zjt_designmanage/zjt_up',
+            data:{id:id,value:value,key:key},
+            success:function(data){
+                location.reload();
+            }
+        })
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   /* if (!location.hash) {
+        location.href = location.pathname + '#list'
+    }
+    $(window).on('hashchange', function () {
+        $('#myTabs li, .panel').removeClass('active');
+        $(location.hash + '-tab').parent().addClass('active');
+        $(location.hash).addClass('active');
+    })
+    $(window).trigger('hashchange');
+    let tbodyEl = $('tbody');
+
+    function render(data) {
+        tbodyEl.empty();
+        data.forEach(function (v, i) {
+            let el = `
+                    <tr data-id="${v.id}">
+                        <td>${v.id}</td>
                         <td>${v.name}</td>
                         <td>${v.pic}</td>
                         <td>${v.content}</td>
@@ -64,5 +171,5 @@ $(function () {
                 }
             })
         }
-    })
+    })*/
 })
